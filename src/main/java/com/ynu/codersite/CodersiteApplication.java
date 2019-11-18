08aac3083a9d479174f70769ff556a4b6494a52d
Jianlong Hu <hujianlongynu@163.com>
2019-11-18 21:10:38 +0800

    初始化

---
+++ src/main/java/com/ynu/codersite/CodersiteApplication.java
@@ -0,0 +1,13 @@
+package com.ynu.codersite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodersiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodersiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ynu/codersite/CodersiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynu.codersite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodersiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
